7fb35a8ad966c475db998dafdc14d803d3a064b4
Shah Fahad <fahadluckyboy43@gmail.com>
2026-04-09 21:52:08 +0300

    first change in the second branch

--- first.js
+++ first.js
@@ -5,3 +5,7 @@ const start = "Let's start exploring Git and its powerful magic.";
 console.log("Made a new branch.");
 
 console.log("I am feature branch.");
+
+function second() {
+  console.log("I am the second branch");
+}
