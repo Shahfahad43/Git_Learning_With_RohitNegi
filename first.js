650e55164424e5ee2e3fadc517a20d725f37f786
Shah Fahad <fahadluckyboy43@gmail.com>
2026-04-10 06:51:27 +0300

    second stash

--- first.js
+++ first.js
@@ -16,4 +16,8 @@ console.log("hello! I am main");
 console.log("hello! I am second");
 
 console.log("I am for making a full request.");
+<<<<<<< Updated upstream
 console.log("I am the third stash.");
+=======
+console.log("I am the fourth stash.");
+>>>>>>> Stashed changes
